59ddfbf0391f157cfb64db8639c237baba63cc35
Kotov Anton <anton1994anton@mail.ru>
2024-04-15 10:23:28 +0400

    [RSFM-2] - Added rename functionality

--- src/1_FileManager/utils/fs/rename.js
+++ src/1_FileManager/utils/fs/rename.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+import fsPromise from 'fs/promises';
+import path from 'path';
+
 const rename = async () => {
-  // Write your code here 
+  const sourceFileName = path.join(
+    'src',
+    '1_FileManager',
+    'files',
+    'wrongFileName.txt'
+  );
+  const resultFileName = path.join(
+    'src',
+    '1_FileManager',
+    'files',
+    'properFileName.md'
+  );
+
+  if (!fs.existsSync(sourceFileName) || fs.existsSync(resultFileName)) {
+    throw new Error('FS operation failed');
+  }
+
+  await fsPromise.rename(sourceFileName, resultFileName);
 };
 
 await rename();
