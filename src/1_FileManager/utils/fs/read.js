59a1b4a5fc635059c2f12b00537da914d65dfc0f
Kotov Anton <anton1994anton@mail.ru>
2024-04-15 10:56:16 +0400

    [RSFM-2] - Added file read functionality

--- src/1_FileManager/utils/fs/read.js
+++ src/1_FileManager/utils/fs/read.js
@@ -1,5 +1,22 @@
+import fsPromise from 'fs/promises';
+import path from 'path';
+import fs from 'fs';
+
 const read = async () => {
-  // Write your code here 
+  const fileToReadPath = path.join(
+    'src',
+    '1_FileManager',
+    'files',
+    'fileToRead.txt'
+  );
+
+  if (!fs.existsSync(fileToReadPath)) {
+    throw new Error('FS operation failed');
+  }
+
+  const fileContents = await fsPromise.readFile(fileToReadPath, { encoding: 'utf8' });
+
+  console.log('fileContents', fileContents);
 };
 
 await read();
