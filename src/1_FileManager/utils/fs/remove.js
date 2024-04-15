4b8b97263753065f03d18766e65888d37ad64af7
Kotov Anton <anton1994anton@mail.ru>
2024-04-15 10:35:20 +0400

    [RSFM-2] - Added functionality for remove

--- src/1_FileManager/utils/fs/remove.js
+++ src/1_FileManager/utils/fs/remove.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import fsPromise from 'fs/promises';
+import path from 'path';
+
 const remove = async () => {
-  // Write your code here 
+  const sourceFileName = path.join(
+    'src',
+    '1_FileManager',
+    'files',
+    'fileToRemove.txt'
+  );
+
+  if (!fs.existsSync(sourceFileName)) {
+    throw new Error('FS operation failed');
+  }
+
+  await fsPromise.unlink(sourceFileName);
 };
 
 await remove();
