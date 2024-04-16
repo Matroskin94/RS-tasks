74263e83d99019473d362d73ae8bd927c244bb08
Kotov Anton <anton1994anton@mail.ru>
2024-04-16 11:06:33 +0400

    [feature/RS-3] - Parsing cli arguments

--- src/2_cli/utils/args.js
+++ src/2_cli/utils/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv.slice(2)
+ 
+  args.forEach((arg, index) => {
+    if (arg.startsWith('--')) {
+      const argName = arg.slice(2)
+      let argValue
+
+      if (index + 1 < args.length) {
+        argValue = args[index + 1].startsWith('--') ? undefined : args[index+1]
+      }
+
+      console.log(`${argName} is ${argValue}`)
+    }
+  })
 };
 
 parseArgs();
