6821860ef7027972f4eaea5914c7eac763add73e
Kotov Anton <anton1994anton@mail.ru>
2024-04-22 12:39:58 +0400

    [feature/RS-7] - Added compress functionality

--- src/6_Compressing/compress.js
+++ src/6_Compressing/compress.js
@@ -1,5 +1,14 @@
+import path from 'path';
+
+import { compressGzip } from './utils/compressGzip.js';
+
 const compress = async () => {
-  // Write your code here 
+  const filesBasePath = path.join('src', '6_compressing', 'files');
+  const fileToCompressPath = path.join(filesBasePath, 'fileToCompress.txt');
+  const archivePath = path.join(filesBasePath, 'archive.gz');
+  await compressGzip(fileToCompressPath, archivePath).catch((e) => {
+    console.log('Compressing error', e);
+  });
 };
 
 await compress();
