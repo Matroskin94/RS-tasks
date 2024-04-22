0d79d601ae4bec23c520622e63606b50fd65fbff
Kotov Anton <anton1994anton@mail.ru>
2024-04-22 10:56:48 +0400

    [RS-6] - Implemented file read using stream

--- src/5_Streams/read.js
+++ src/5_Streams/read.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
 const read = async () => {
-  // Write your code here 
+  const fileToReadPath = path.join(
+    'src',
+    '5_Streams2',
+    'files',
+    'fileToRead.txt'
+  );
+
+  const readableStream = fs.createReadStream(fileToReadPath, 'utf-8')
+
+  readableStream.pipe(process.stdout)
+  readableStream.on('error', (e) => {
+    console.log('readable stream error: ', e)
+  })
+
+  readableStream.close()
 };
 
 await read();
