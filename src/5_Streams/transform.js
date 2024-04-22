a8e8bb30439453f398c345c8c07b6c5377118189
Kotov Anton <anton1994anton@mail.ru>
2024-04-22 11:31:24 +0400

    [feature/RS-6] - Added transform implementation

--- src/5_Streams/transform.js
+++ src/5_Streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-  // Write your code here
+  const reverseStream = new Transform({
+    transform(data, encoding, callback) {
+      const reversedData = data.toString().split('').reverse().join('');
+      this.push(reversedData);
+      callback();
+    },
+  });
+  
+  process.stdin.pipe(reverseStream).pipe(process.stdout)
 };
 
 await transform();
