78a4baaaa5fc684d96ebbd1cbf0dd3bd8ce6625e
Kotov Anton <anton1994anton@mail.ru>
2024-04-25 15:15:40 +0400

    [feature/RS-10] - Implemented IPC-channel

--- src/8_Child_process/cp.js
+++ src/8_Child_process/cp.js
@@ -1,6 +1,12 @@
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const childProcess = spawn(
+    'node ./src/8_Child_process/files/script.js',
+    args,
+    { stdio: [process.stdin, process.stdout], shell: true }
+    // `subprocess.stdin`,`subprocess.stdout`, and `subprocess.stderr`
+  );
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1']);
